e84d71089eb5fa5547f57c76b6fe21457519193b
vikas201819 <mauryavikas83@gmail.com>
2025-01-27 18:09:20 +0530

    done

--- main.tf
+++ main.tf
@@ -1,66 +1,51 @@
-provider "azurerm" {
-    features {}
+provider: azurerm{
+  features{}
 }
 
-resource "azurerm_resource_group" "example" {
-    name     = "example-resources"
-    location = "East US"
-}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  resource_group_name = var.resourcesrg
+  location            = var.location
+  address_space       = ["10.0.0.0/16"]
 
-resource "azurerm_virtual_network" "example" {
-    name                = "example-network"
-    address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.example.location
-    resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-    name                 = "example-subnet"
-    resource_group_name  = azurerm_resource_group.example.name
-    virtual_network_name = azurerm_virtual_network.example.name
-    address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-    name                = "example-nic"
-    location            = azurerm_resource_group.example.location
-    resource_group_name = azurerm_resource_group.example.name
-
-    ip_configuration {
-        name                          = "internal"
-        subnet_id                     = azurerm_subnet.example.id
-        private_ip_address_allocation = "Dynamic"
-    }
-}
-
-resource "azurerm_virtual_machine" "example" {
-    name                  = "example-machine"
-    location              = azurerm_resource_group.example.location
-    resource_group_name   = azurerm_resource_group.example.name
-    network_interface_ids = [azurerm_network_interface.example.id]
-    vm_size               = "Standard_DS1_v2"
-
-    storage_os_disk {
-        name              = "example-os-disk"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Standard_LRS"
+    tags = {
+        environment = "Production"
     }
 
-    storage_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
+    resource "azurerm_windows_virtual_machine" "vm" {
+        name                  = "vm"
+        resource_group_name   = azurerm_resource_group.rg.name
+        location              = azurerm_resource_group.rg.location
+        size                  = "Standard_F2"
+        admin_username        = "adminuser"
+        admin_password        = "Password1234!"
+        network_interface_ids = [azurerm_network_interface.nic.id]
+        os_disk {
+            caching              = "ReadWrite"
+            storage_account_type = "Standard_LRS"
+        }
+
+        source_image_reference {
+            publisher = "MicrosoftWindowsServer"
+            offer     = "WindowsServer"
+            sku       = "2019-Datacenter"
+            version   = "latest"
+        }
+
+        tags = {
+            environment = "Production"
+        }
     }
 
-    os_profile {
-        computer_name  = "hostname"
-        admin_username = "adminuser"
-        admin_password = "Password1234!"
+    resource "azurerm_network_interface" "nic" {
+        name                = "nic"
+        resource_group_name = azurerm_resource_group.rg.name
+        location            = azurerm_resource_group.rg.location
+
+        ip_configuration {
+            name                          = "nicConfiguration"
+            subnet_id                     = azurerm_subnet.subnet.id
+            private_ip_address_allocation = "Dynamic"
+        }
     }
 
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
-}
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable: "resourcesrg"
+default: "resources-rg"
+description: "The name of the resource group to deploy the resources to."
+
+variable: "location"
+default: "East US"
+description: "The location of the resource group to deploy the resources to."
+
+variable: "vnetname"
+default: "vnet"
+description: "The name of the virtual network to deploy the resources to."
+
